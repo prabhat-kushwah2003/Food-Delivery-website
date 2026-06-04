a2129732a175adc1a73dbabe89ed2382cdde3667
prabhat-kushwah2003 <prabhatkushwah2003@email.com>
2026-06-04 16:20:24 +0530

    url updated

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider =(props)=>{
 
     
     const [cartItems,setCartItems] = useState({});  
-    const url = "http://localhost:4000"
+    const url = import.meta.env.VITE_API_URL;
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([]);
 
